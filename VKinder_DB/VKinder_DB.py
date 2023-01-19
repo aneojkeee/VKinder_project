ad94b596d195d6ad4401be6674e7abdf3e4d914d
Alexey S <113237660+alex1shad@users.noreply.github.com>
2023-01-19 21:34:48 +0300

    create_db
    Добавил скрипт для создания базы данных и работы с ними. Никаких баз данных на ПК создавать не нужно, лишь установите нужные библиотеки из файла requirements.txt и поменяйте настройки в файле config.json, база данных создастся сама.

---
+++ VKinder_DB/VKinder_DB.py
@@ -0,0 +1,149 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+import sqlalchemy
+import json
+import models as m
+
+
+with open('config_db.json', 'r') as user_file:
+    db_type, login, password, hostname, db_port, db_name = json.load(user_file).values()
+
+DSN = f'{db_type}://{login}:{password}@{hostname}:{db_port}/{db_name}'
+engine = sqlalchemy.create_engine(DSN)
+if not database_exists(engine.url):
+    create_database(engine.url)
+m.create_table(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def add_user(vk_user_id, first_name, sex, age, city):
+    '''
+        В данном случае user - это пользователь, который ищет пару.
+    Для простоты будем называть его "пользователь".
+    :param vk_user_id: id пользовател
+    :param first_name: имя пользователя. Имя нужно лишь для обращения к пользователю при общении с ботом
+    :param sex: пол пользователя. 0 - женский, 1 - мужской
+    :param age: возраст пользователя
+    :param city: город пользователя
+    '''
+    user_find = session.query(m.User.vk_user_id).all()
+    if vk_user_id not in [user[0] for user in user_find]:
+        user = m.User(vk_user_id=vk_user_id, first_name=first_name, sex=sex, age=age, city=city)
+        session.add(user)
+        session.commit()
+
+
+def add_offer(vk_user_id, vk_offer_id, first_name, last_name, sex, age, city, black_list=0):
+    '''
+        В данном случае offer - это предложенный пользователю человек из поиска.
+    Для простоты будем называть его "предложением".
+    :param vk_user_id: id предложения
+    :param first_name: имя предложения
+    :param last_name: фамилия предложения
+    :param sex: пол предложения. 0 - женский, 1 - мужской
+    :param age: возраст предложения
+    :param city: город предложения
+    :param black_list: если пользователь больше не хочет видеть предложение, предложение скрывается из поиска навсегда.
+                        0 - предложение актуально, 1 - предложение исключено (исключается) из поиска
+    '''
+    offer_find = session.query(m.Offer.vk_offer_id).all()
+    if vk_offer_id not in [offer[0] for offer in offer_find]:
+        offer = m.Offer(vk_offer_id=vk_offer_id, first_name=first_name, last_name=last_name, sex=sex, age=age, city=city)
+        session.add(offer)
+    user_offer_find = session.query(m.User_Offer.user_offer_id).\
+        filter(m.User_Offer.vk_user_id == vk_user_id).\
+        filter(m.User_Offer.vk_offer_id == vk_offer_id).all()
+    if len(user_offer_find) == 0:
+        user_offer = m.User_Offer(vk_user_id=vk_user_id, vk_offer_id=vk_offer_id, black_list=black_list)
+        session.add(user_offer)
+    elif black_list == 1:
+        session.query(m.User_Offer).\
+            filter(m.User_Offer.vk_offer_id == vk_offer_id).\
+            filter(m.User_Offer.vk_user_id == vk_user_id).\
+            filter(m.User_Offer.black_list == 0).\
+            update({'black_list': black_list})
+    session.commit()
+
+
+def add_photo(vk_offer_id, photo_url):
+    '''
+    :param vk_offer_id: id предложения
+    :param photo_url: лист со ссылками на фотографии
+    '''
+    for url in photo_url:
+        photo = m.Photo(vk_offer_id=vk_offer_id, photo_url=url)
+        session.add(photo)
+    session.commit()
+
+
+def add_interest(interest, vk_user_id=0, vk_offer_id=0):
+    '''
+    :param interest: наименование интереса пользователя или предложения
+    :param vk_user_id: id пользователя
+    :param vk_offer_id: id предложения
+    '''
+    interest_find = session.query(m.Interest.interest).filter(m.Interest.interest == interest)
+    if interest not in [interest[0] for interest in interest_find]:
+        interest_add = m.Interest(interest=interest)
+        session.add(interest_add)
+    interest_id_find = session.query(m.Interest.interest_id).filter(m.Interest.interest == interest).all()[0][0]
+    user_find = session.query(m.Interest_Person.vk_user_id).\
+        filter(m.Interest_Person.interest_id == interest_id_find).\
+        filter(m.Interest_Person.vk_user_id == vk_user_id).all()
+    offer_find = session.query(m.Interest_Person.vk_offer_id).\
+        filter(m.Interest_Person.interest_id == interest_id_find).\
+        filter(m.Interest_Person.vk_offer_id == vk_offer_id).all()
+    if vk_user_id != 0 and vk_user_id not in [user[0] for user in user_find]:
+        interest_person_add = m.Interest_Person(vk_user_id=vk_user_id, interest_id=interest_id_find)
+        session.add(interest_person_add)
+    if vk_offer_id != 0 and vk_offer_id not in [offer[0] for offer in offer_find]:
+        interest_person_add = m.Interest_Person(vk_offer_id=vk_offer_id, interest_id=interest_id_find)
+        session.add(interest_person_add)
+    session.commit()
+
+
+def get_offer(vk_user_id):
+    '''
+    :param vk_user_id: id пользователя
+    :return: лист, содержащий листы со сведениями о предложении.
+            Формат листа:
+            [[предложение 1], [предложение 2], ..., [предложение n]]
+            Формат предложения:
+            [ [id предложения, 'имя', 'фамилия', пол, возраст, 'город', отметка черного списка,
+            [лист со ссылками на фотографии], [лист с общими интересами пользователя и предложения] ]
+    '''
+    offer = session.query(m.Offer.vk_offer_id,
+                          m.Offer.first_name,
+                          m.Offer.last_name,
+                          m.Offer.sex,
+                          m.Offer.age,
+                          m.Offer.city,
+                          m.User_Offer.black_list).\
+        join(m.User_Offer, m.User_Offer.vk_offer_id == m.Offer.vk_offer_id).\
+        join(m.User, m.User.vk_user_id == m.User_Offer.vk_user_id).\
+        filter(m.User.vk_user_id == vk_user_id).\
+        filter(m.User_Offer.black_list == 0).all()
+    offer_list = []
+    user_interests = session.query(m.Interest.interest). \
+        join(m.Interest_Person, m.Interest_Person.interest_id == m.Interest.interest_id). \
+        filter(m.Interest_Person.vk_user_id == vk_user_id).all()
+    for note in offer:
+        offer_list.append([])
+        for el in note:
+            offer_list[-1].append(el)
+        photo = session.query(m.Photo.photo_url).filter(m.Photo.vk_offer_id == note[0]).all()
+        offer_list[-1].append([url[0] for url in photo])
+        offer_interests = session.query(m.Interest.interest). \
+            join(m.Interest_Person, m.Interest_Person.interest_id == m.Interest.interest_id). \
+            filter(m.Interest_Person.vk_offer_id == note[0]).all()
+        interest_list = []
+        for interest in [inter_user[0] for inter_user in user_interests]:
+            if interest in [inter_offer[0] for inter_offer in offer_interests]:
+                interest_list.append(interest)
+        offer_list[-1].append(interest_list)
+    return offer_list
+
+
+session.close()
