7845d3047b205f6933ed7c25f9e930326c7115ae
Alexey S <113237660+alex1shad@users.noreply.github.com>
2023-01-23 21:10:17 +0300

    new_branch_commit

---
+++ VKinder_DB_new/VKinder_get_and_filling/VKinder_get_data.py
@@ -0,0 +1,200 @@
+import requests
+import traceback
+import time
+import logging
+from datetime import date
+from datetime import datetime
+
+
+logging.basicConfig(level=logging.INFO, filename="main.log", filemode="w",
+                    format="%(asctime)s %(levelname)s %(message)s")
+
+
+class VKinder:
+    """Класс взаимодействующий с VK API"""
+
+    dict_get_user = {}
+    list_search_user = []
+    list_photos_search_user = []
+
+    def __init__(self, token, user_id):
+        """
+        :param token: Токен VK Api
+        :param user_id: id пользователя, который пишет нам в бот
+        """
+        self.user_id = user_id
+        self.params = {
+            'access_token': token,
+            'v': '5.131'
+            }
+
+    def http_exception(self, status, message=''):
+        """Функция исключения, выбрасываем, когда API возвращает ошибку"""
+        return f'http error: {status}\n{message}'
+
+    def _send_request(self, http_method, uri_path, params=None, json=None, response_type=None):
+        """
+        Через этот метод будут отправляться все запросы ко всем API.
+        Здесь мы можем обрабатывать любые исключения, логирвать запросы и т.п.
+
+        :param http_method: GET/POST/PUT/PATCH/DELETE
+        :param uri_path: uri API, например method/users.get
+        :param params:
+        :param json:
+        :param response_type: тип ответа, например json
+        :return:
+        """
+        host = 'https://api.vk.com/method'
+        response = requests.request(http_method, f'{host}/{uri_path}', params=params, json=json)  # отправляем запрос
+        logging.info(f"""
+                        Date and time: {datetime.now().strftime('%Y-%m-%d %H.%M')}
+                        Method: {host}/{uri_path}
+                        Status_code: {response.status_code}
+                        Params: {params}
+                        """)
+        if response.status_code >= 400:
+            # если с сервера приходит ошибка выбрасываем исключение
+            raise self.http_exception(response.status_code, response.text)
+        if response_type == 'json':
+            response = response.json()
+        return response
+
+    def user_age(self, user_b_date):
+        date_born_str = user_b_date['bdate']
+        date_born_list = [int(el) for el in date_born_str.split('.')][::-1]
+        date_born = date(*date_born_list)
+        delta_years = date.today().year - date_born.year
+        if (date.today().month, date.today().day) > (date_born.month, date_born.day):
+            return delta_years
+        else:
+            return delta_years - 1
+
+    def get_user(self):
+        """
+        Получаем пользователя, используя унаследованный метод _send_request и передаем словарь
+        в датакласс AllInfo
+        """
+        res = self._send_request(
+            http_method='GET',
+            uri_path='users.get',
+            params={
+                'user_id': self.user_id,
+                'fields': 'sex,city,bdate',
+                **self.params
+                    },
+            response_type='json'
+        )
+        u_bdate = res['response'][0]
+        u_city = res['response'][0]['city']['id']
+        u_sex = res['response'][0]['sex']
+        u_name = res['response'][0]['first_name']
+        self.dict_get_user = {'first_name': u_name,
+                     'b_date': self.user_age(u_bdate),
+                     'city': u_city,
+                     'sex': u_sex,
+                     'city_name': res['response'][0]['city']['title']}
+
+    def search_user(self):
+        """
+        Используем информацию, полученную от метода get_user(пол, возраст, город пользователя общающегося с ботом)
+        из датакласса AllInfo.
+        Через унаследованный метод _send_request, передаем VK параметры поиска людей для знакомства,
+        получаем список словарей с данными предложений подходящих пользователю.
+        Передаем эту информацию в датакласс AllInfo.
+        """
+
+        age = self.dict_get_user['b_date']
+
+        res = self._send_request(
+            http_method='GET',
+            uri_path='users.search',
+            params={'count': 15, # Количество пользователь которых хотим найти, поменять на 1000 как закончим тесты
+                    'fields': 'city,bdate,sex,counters',
+                    'city': self.dict_get_user['city'],
+                    'sex': 2 if self.dict_get_user['sex'] == 1 else 1,
+                    'age_from': age - 5,
+                    'age_to': age + 5,
+                     'status': '6',
+                     **self.params},
+            response_type='json'
+        )
+
+        for user in res['response']['items']:
+            if user['is_closed'] == True:
+                continue
+            if 'city' not in user.keys():
+                continue
+            if user['city']['id'] != self.dict_get_user['city']:
+                continue
+            self.list_search_user.append(
+                {'id_offer': user['id'],
+                 'first_name': user['first_name'],
+                 'last_name': user['last_name'],
+                 'sex': user['sex'],
+                 'city': user['city']['title'],
+                 'bdate': self.user_age(user)
+                 })
+
+    def get_fotos_user(self):
+        """
+        Используем информацию, полученную от метода search_user(а именно список людей
+        предложенных пользователю для знакомства) из датакласса AllInfo.
+        Осуществляем отбор 3 топовых фото по количеству лайков.
+        Передаем список словарей с данными которые буду выводиться в чате с ботом пользователю.
+        Имя, Фамилия, ссылка на профиль, 3 ссылки на фото предложения.
+        Так же передаем эти данные в датакласс AllInfo.
+        """
+        for data in self.list_search_user:
+            res = self._send_request(
+                http_method='GET',
+                uri_path='photos.get',
+                params={'owner_id': data['id_offer'],
+                        'album_id': 'profile',
+                        'extended': 1,
+                        'count': 15,
+                        **self.params},
+                response_type='json'
+            )
+            time.sleep(0.2) #Чтобы сервер не блокировал из-за привышения лимита запросов
+
+            if res['response']['count'] == 0:
+                continue
+
+            list_id_likes = []
+            for i in res['response']['items']:
+                url_photo = i['sizes'][-1]['url']
+                likes = i['likes']['count']
+                list_id_likes.append({'url': url_photo, 'likes': likes})
+            list_id_likes.sort(key=lambda dictionary: dictionary['likes'])
+
+            if len(list_id_likes) >= 3:
+                list_top_photos = [list_id_likes[-1]['url'], list_id_likes[-2]['url'], list_id_likes[-3]['url']]
+            else:
+                list_top_photos = [url['url'] for url in list_id_likes]
+            self.list_photos_search_user.append({'id_offer': data["id_offer"],
+                                                'top_photos': list_top_photos,
+                                                'profile': f'https://vk.com/id{data["id_offer"]}'})
+
+    def activate_get_search_photos(self):
+        """
+        Метод активирует, все предыдущие методы данного класса и проверяет на наличие даты рождения у пользователя
+        """
+        try:
+            self.get_user()
+            self.search_user()
+            self.get_fotos_user()
+            return self.dict_get_user, self.list_search_user, self.list_photos_search_user
+        except KeyError:
+            if 'bdate' in traceback.format_exc():
+                print('Дата рождения отсутствует')
+
+    def clear_func(self):
+        self.user_id = 0
+        self.dict_get_user = {}
+        self.list_search_user = []
+        self.list_photos_search_user = []
+
+
+# user_id = 1
+# vkinder = Vkinder(token, user_id)
+# print(vkinder.activate_get_search_photos())
---
+++ VKinder_DB_new/VKinder_get_and_filling/tokens_vk.py
@@ -0,0 +1,8 @@
+#Токен от бота
+token_gr = 'vk1.a.djuK5umcqmDrHBiAI-BlG9YbMySz4TuGdnSyOZNMNzvJVXx-IwdsFtNejOSZ8CkWE2p0hyy2QqocRrF4r-4MUjpClZZh-PFSkOQ8ZyUuNVyd6dF3e18LOTpeoiRg78OqlnDWG7r2tppWS7P9fXY2ltp48EZLktyAwMsW1bNnEdea6o-IRwVZTKfQfUoimtMdrzZiQMBDBgnN44Ey6Om-9A'
+
+
+#Токен пользователя сгенерированный по
+#https://oauth.vk.com/authorize?client_id={client_id}&scope=65536&response_type=token
+#Вместо {client_id} указываем id от своего приложения
+token = 'vk1.a.cK0U-3zNjP7muvUtn6cASIUGxNeFy6UoAiHcTWvIh3eo8l6SHLjy-3pb6hfglQ_wRTqkrwED9sye2169-4UlR_If7DHOlhRZjdWT_8gpQBmLQWMp15idRy_t0zygVPjW6yKL7D877s_2iT6aRqLJ9U5gI6u-aKEKx6CpP2vSEKb-ojJVghMFaGXFjROVfbpHWJwbeyFF2GANLZ6r2UVQKA'
